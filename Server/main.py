7ce351fff7e75ba281d7cc58854da45089f598ca
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2021-04-21 00:32:17 +0900

    changed PySide2

--- Server/main.py
+++ Server/main.py
@@ -2,20 +2,28 @@
     メイン画面表示部分
 """
 
-from py_modules import NFCRead_dummpy
+from py_modules import NFCRead_dummy
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel
-from PyQt5.QtCore import Qt, QTimer
-from PyQt5.GtGui import QFont
+from PySide2 import QtWidgets
 
-app = QApplication([])
-window = Qwidget()
-layout = QGridLayout()
-label = QLabel("出席", window)
-label.setAlignment(Qt.AlignVCenter | Qt.AlignHCenter)
-label.setFont(QFont("Monospace Regular", 16, QFont.Bold))
-layout.addWidget(label, 0, 0)
-window.setLayout(layout)
-QTimer.singleShot(2000, window.close)
-sys.exit(app.exec_())
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Team7 - Main")
+
+app = QtWidgets.QApplication()
+window = MainWindow()
+window.show()
+app.exec_()
+
+# app = QApplication([])
+# window = Qwidget()
+# layout = QGridLayout()
+# label = QLabel("出席", window)
+# label.setAlignment(Qt.AlignVCenter | Qt.AlignHCenter)
+# label.setFont(QFont("Monospace Regular", 16, QFont.Bold))
+# layout.addWidget(label, 0, 0)
+# window.setLayout(layout)
+# QTimer.singleShot(2000, window.close)
+# sys.exit(app.exec_())
