856532279cde5caaf425edf2520cc8aef2f98ec9
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2021-05-25 01:10:12 +0900

    reconstruct
    Removed the included Node module.

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,17 @@
+/*
+    Team7 server js | Update: 2021/05/25
+    Our project: https://github.com/Fukuda-B/Team7
+
+    Memo:
+    Authentication middleware: [passport](https://github.com/jaredhanson/passport])
+    Template engine: [ejs](https://ejs.co/)
+*/
+
+'use strict'
 var createError = require('http-errors');
 var cookieParser = require('cookie-parser');
-var cookieSettion = require('coolie-session');
-var pug = require('pug');
+// var cookieSettion = require('cookie-session');
+var ejs = require('ejs');
 var byrypt = require('bcrypt');
 var express = require('express');
 var compression = require('compression');
@@ -11,10 +21,10 @@ var passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy;
 
 // -----
-
+// Router module
 var index = require('./routes/index');
-var api_root = require('./routes/api');
-var login = require('./routes/login');
+// var api_root = require('./routes/api/v1/team7');
+// var login = require('./routes/login');
 
 // -----
 var app = express();
@@ -35,11 +45,14 @@ app.use(
     })
 );
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// Template engine & Req Middle
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
+app.use(express.static(path.join(__dirname, './public')));
 
+/*
 app.use(passport.initialize());
 passport.use(new LocalStrategy({
     usernameFil: "username",
@@ -47,14 +60,17 @@ passport.use(new LocalStrategy({
 }, funciton(username, password, done) {
 }));
 passport.use(new LocalStrategy(function(username, passport, done){
-
 }))
-app.use(logger('dev'));
+*/
+
+// app.use(logger('dev'));
+app.use(morgan({format: 'dev', immediate: true}));
 app.use(cookieParser());
 
+// Router setting
 app.use('/', index);
-app.use('/api', api_root);
-app.use('/login', login);
+// app.use('/api/v1/team7', api_root);
+// app.use('/login', login);
 
 app.user(function(req, res, next) {
     next(createError(404));
@@ -67,4 +83,3 @@ app.user(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
-module.exports = app;
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,6 +1,32 @@
+/*
+    Team7 server js | Update: 2021/05/25
+*/
+
+'use strict'
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+var var_pack = {
+    title: 'Team7',
+}
 
-});
+router
+    // GET req
+    .get('/', (req, res) => {
+        res.render('index', var_pack)
+    })
+    .get('/login', (req, res) => {
+        res.render('login', var_pack)
+    })
+    .get('/api/v1/team7', (req, res) => {
+        res.render('api', var_pack)
+    })
+
+    // POST req
+    .post('/api/v1/team7', function(req, res) {
+        var _username = req.body.username;
+        var _userid = req.body.userid;
+        res.render('api', {...var_pack, ...{username: _username}});
+    })
+
+module.exports = app;
