b19f013915676d6f4da1d5adcfb77828e2fd9ab5
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2021-05-26 00:22:04 +0900

    make function

--- Server/routes/home.js
+++ Server/routes/home.js
@@ -8,20 +8,25 @@ var router = express.Router();
 var fs = require('fs');
 
 var jsonFile = './routes/test_json.json';
-var lecture_json = JSON.parse(fs.readFileSync(jsonFile, 'utf8'));
 
-var lecture_table = '',
+function createTable(lecture_json) {
+    var lecture_table = '',
     tmp = '';
-for(var i=0; i<lecture_json.lecture.length; i++){
-    tmp = lecture_json.lecture[i];
-    // console.log(tmp.lecture_date);
-    lecture_table += '<tr><td>'+tmp.lecture_name
-        +'</td><td>'+tmp.lecture_date.f+' '
-        +tmp.lecture_date.w+' '
-        +tmp.lecture_date.t
-        +'</td><td>'+tmp.lecture_teach
-        +'</td><td>'+tmp.lecture_par+'</td></tr>';
+    for(var i=0; i<lecture_json.lecture.length; i++) {
+        tmp = lecture_json.lecture[i];
+        // console.log(tmp.lecture_date);
+        lecture_table += '<tr><td>'+tmp.lecture_name
+            +'</td><td>'+tmp.lecture_date.f+' '
+            +tmp.lecture_date.w+' '
+            +tmp.lecture_date.t
+            +'</td><td>'+tmp.lecture_teach
+            +'</td><td>'+tmp.lecture_par+'%</td></tr>';
+    }
+    return lecture_table;
 }
+
+var lecture_json = JSON.parse(fs.readFileSync(jsonFile, 'utf8'));
+var lecture_table = createTable(lecture_json);
 
 router
     // GET req
