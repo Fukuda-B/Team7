4f928819a14e70f54bdf125466109375eb6245e9
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2021-05-26 23:28:23 +0900

    add electron

---
+++ Electron/main.js
@@ -0,0 +1,29 @@
+/*
+    Team7 electron | Update: 2021/05/26
+
+*/
+
+'use strict';
+
+var electron = require('electron');
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+let mainWindow; // Don't overwrite this.
+
+// if close all window, exit program.
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+});
+  
+// start electron
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        width: 1000, height: 720,
+        webPreferences: { nodeIntegration : false } // DbXSS
+    });
+    mainWindow.loadURL('http://localhost:3000/');
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+});
