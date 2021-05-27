4f92d80a8354a3d3d217fc327fcfdde376e9f56c
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2021-05-27 14:23:28 +0900

    update server

--- Electron/main.js
+++ Electron/main.js
@@ -14,7 +14,7 @@ let mainWindow; // Don't overwrite this.
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
 });
-  
+
 // start electron
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
--- Server/routes/login.js
+++ Server/routes/login.js
@@ -11,25 +11,50 @@ var passport = require('passport');
 var crypto = require('crypto');
 var router = express.Router();
 
-/*
-    Team7 will use ECDH P-521 for key generation and AES256 for encryption.
-    When using multiple algorithms, the cipher strength depends on the one with lower cipher strength.
-    In this case, ECDH has a lower strength. (Strength with the same key length: ECDH < AES)
-    So, ECDH requires a key length of 512 bits or more to match the 256 bits of AES.
+var CRYPTOS = {
+    /*
+        Team7 will use ECDH P-521 for key generation and AES256 for encryption.
+        When using multiple algorithms, the cipher strength depends on the one with lower cipher strength.
+        In this case, ECDH has a lower strength. (Strength with the same key length: ECDH < AES)
+        So, ECDH requires a key length of 512 bits or more to match the 256 bits of AES.
 
-    crypto.subtle.generateKey(algorithm, exportable, usage)
-*/
-function key_gen() {
-    var crypto_v = {
-        name: "ECDH",
-        namedCurve: "P-521"
-    }
-    var crypto_u = ["deriverKey"];
-    return crypto.subtle.generateKey(crypto_v, false, crypto_u);
+        crypto.subtle.generateKey(algorithm, exportable, usage)
+    */
+    KEY_GEN : function() {
+        var crypto_v = {
+            name: "ECDH",
+            namedCurve: "P-521"
+        }
+        var crypto_u = ["deriverKey"];
+        return crypto.subtle.generateKey(crypto_v, false, crypto_u);
+    },
+
+    // Generating a common key
+    KEY_GEN_C : function() {
+        var gkey = CRYPTOS.KEY_GEN();
+        var aes = {
+            name: "ARS-GCM",
+            length: 256
+        };
+        var ecdh = {
+            name: "ECDH",
+            public: gkey.publickey
+        };
+        var crypto_u = ["encrypt", "decrypt"];
+        return crypto.subtle.deriveKey(ecdh, gkey.publickey, aes, false, crypto_u);
+    },
+
+    // Decrypto from AES
+    DECRYPTO : function(key, val) {
+            var aes = {
+                name: "AES-GCM",
+                iv: val.subarray(0, 16),
+                tagLength: 256
+            };
+        return new Uint8Array(await crypto.subtle.decrypto(aes, key , val.subarray(16)));
+    },
 }
-function decrypto() {
-    return
-}
+
 
 router
     // GET req
