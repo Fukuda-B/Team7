d182f87b42ef7f2ff1778efd1fb2191b31622e30
Fukuda-B <fukuda.p20@gmail.com>
2021-05-30 21:04:54 +0900

    add update_iv()

--- Server/routes/login.js
+++ Server/routes/login.js
@@ -6,6 +6,7 @@
     https://http2.try-and-test.net/ecdhe.html
     https://qiita.com/angel_p_57/items/2e3f3f8661de32a0d432
     https://stackoverflow.com/questions/8855687/secure-random-token-in-node-js
+    https://ameblo.jp/reverse-eg-mal-memo/entry-12580058952.html
 
 
 -----
@@ -20,6 +21,7 @@
 
     暗号化 ⇔ 復号 (復号化ではない)
     パティング: 暗号アルゴリズムによるが、データ長はある値の倍長である必要があるときに余分な追加される
+    サイドチャネル攻撃
 */
 
 var express = require('express');
@@ -30,15 +32,18 @@ var router = express.Router();
 var key_size = 2<<6; // 2<<7=128, key.length=256
 var key_timeout = 10*1000; // ms
 
+// Generate key
+let key_v_gen = () => crypto.randomBytes(key_size).toString('hex');
+// Generate iv
+let iv_v_gen = () => crypto.randomBytes(key_size).toString('hex');
 // Generating a encryption key
 function key_gen() {
-    var iv_val = crypto.randomBytes(key_size).toString('hex');
-    var key_val = crypto.randomBytes(key_size).toString('hex');
     return {
-        "iv": iv_val,
-        "key": key_val,
+        "iv": iv_v_gen(),
+        "key": key_v_gen(),
     }
 }
+
 // Decrypt
 function decryptoo(data, bank) {
     // var encryptedHexStr = CryptoJS.enc.Hex.parse(data);
@@ -65,7 +70,12 @@ function decryptoo2(data, bank) {
 
 // Update the encryption key periodically.
 var bank = key_gen();
-// setTimeout(() => { bank = key_gen(); }, key_timeout);
+var update_iv = () => {
+    bank.iv = iv_v_gen();
+    console.log(bank.iv);
+    setTimeout(update_iv, key_timeout);
+}
+update_iv();
 
 // Request
 router
@@ -76,6 +86,9 @@ router
             crypto_bank: bank,
         });
     })
+    .get('/u', function (req, res) {
+        res.send(bank.iv);
+    })
     // POST req
     .post('/', function (req, res) {
         if (req.body.team7) {
@@ -84,9 +97,11 @@ router
                 // var body = req.body.team7;
                 console.log({'req':body, 'bank':bank});
                 var data = decryptoo(body, bank);
-                console.log(data);
+                console.log('>> '+data);
+                res.send('b');
             } catch (error) {
                 console.log(error);
+                res.send('d');
             }
         }
     });
