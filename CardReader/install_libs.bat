302930476fc4fbfa80af892cd8b819f8b9c60014
Fukuda-B <fukuda.p20@gmail.com>
2021-06-04 16:42:02 +0900

    Create install_libs.bat

---
+++ CardReader/install_libs.bat
@@ -0,0 +1,11 @@
+@echo off
+title Team7 - CardReader
+rem|____________________________________________________________
+rem|   Team7 Portable | Install Py library | Update: 2021/06/04
+rem
+rem|   Requirements: Python & pip
+rem|   Place this .bat file in the same directory as Team7/CardReader
+rem|____________________________________________________________
+
+rem| install py library
+call pip install -r requirements.txt
