37a3011491726d87d2337c1e587337bc109fd31f
Fukuda-B <fukuda.p20@gmail.com>
2021-07-11 21:53:05 +0900

    fixed to work

--- CardReader/display.py
+++ CardReader/display.py
@@ -1,13 +1,12 @@
 import sys
-from PyQt5.QtWidgets 
-import GApplication, QWidget, QGridLayout, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QFont
 
 app = QApplication([])
 window = QWidget()
 layout = QGridLayout()
-label = Qlabel("出席",window)
+label = QLabel("出席",window)
 label.setAlignment(Qt.AlignVCenter | Qt.AlignHCenter)
 label.setFont(QFont("Monospace Regular",100,QFont.Bold))
 layout.addWidget(label,0,0)
