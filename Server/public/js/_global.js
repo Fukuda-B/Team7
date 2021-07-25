c1a9e32d45f852b3daf7398a5e41425923ba83d6
Fukuda-B <fukuda.p20@gmail.com>
2021-07-25 16:00:31 +0900

    update version

--- Server/public/js/_global.js
+++ Server/public/js/_global.js
@@ -3,6 +3,6 @@
 */
 
 (function () {
-  console.log("%cTeam7%c v0.7.3 - 20210721", style = "font-size: 1.5em;", style = "color:orange;");
+  console.log("%cTeam7%c v0.9.2 - 20210725", style = "font-size: 1.5em;", style = "color:orange;");
   console.log("%cWelcome (・∀・)/%c", style = "color:#888;", "");
 }());
